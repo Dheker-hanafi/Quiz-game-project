ea42a11456ce6379b4a4e3f236fef11ff21f18ac
Dheker-hanafi <dhekerhanafi@gmail.com>
2023-11-27 20:47:54 +0100

    new change

--- code.js
+++ code.js
@@ -1,36 +1,84 @@
 // {
-//     theme:"",
-//     question:"",
-//     answers:[{a1:"",value:},{a2:"",value:},{a3:"",value:},{a4:"",value:}]
+//      theme:"",
+//      question:"",
+//     a:"",
+//        b:"",
+//        c:"",
+//        d:"",
+//        correct:""
 // }
 const questions=[
     {
         theme:"history",
         question:"which famous Carthaginian leader fought in the scond punic war",
-        answers:[{a1:"hannibal",value:true},{a2:"hamilcar",value:false},{a3:"jules cesar",value:false},{a4:"scipio",value:false}]
+        a:"hannibal",
+        b:"hamilcar",
+        c:"jules cesar",
+        d:"scipio",
+        correct:"hannibal"
     },
     {
         theme:"sport",
         question:"what year did Tunisia win the africa cup of nations",
-        answers:[{a1:"2002",value:false},{a2:"2004",value:true},{a3:"2006",value:false},{a4:"2008",value:false}]
+        a:"2002",
+        b:"2004",
+        c:"2006",
+        d:"2008",
+        correct:"2004"
     },
     {
         theme:"science",
         question:"which scientisit discovered Antibiotics",
-        answers:[{a1:"Louis Pasteur",value:false},{a2:"Alexander Fleming",value:true},{a3:"Gregor Mendell",value:false},{a4:"Stephen Hawking",value:false}]
+        a:"Louis Pasteur",
+        b:"Alexander Fleming",
+        c:"Gregor Mendell",
+        d:"Stephen Hawking",
+        correct:"Alexander Fleming"
     }
 ]
 
-let question=$('#question')
-let index
-function start() {
+let index =0
+let score =0
+function startQuiz() {
     index = 0
     score = 0
+    questions.sort(() => Math.random() - 0.5);
+    $("#quiz_screen").show()
+    $("#welcome_screen").hide()
+    console.log("hello");
     displayQuestion()
-    updateScore()
 }
 
-function test () {
-    console.log('tex')
- }
+function displayQuestion() {
+    let currentQuestion = questions[index]
+
+    $("#question").append(currentQuestion.question)
+    
+    $(".key1").append(currentQuestion.a)
+    $(".key2").append(currentQuestion.b)
+    $(".key3").append(currentQuestion.c)
+    $(".key4").append(currentQuestion.d)
+    
+}
+
+function checkResult(){
+
+    
+}
+
+function showResult() {
+    $("#quiz-screen").hide();
+    $("#result-screen").show();
+    $("#score").text(score);
+}
+
+
+function resetQuiz(){
+    index=0
+    score=0
+
+    questions.sort(() => Math.random() - 0.5)
+    $("#quiz_screen").hide();
+    $("#welcome_screen").show();
+}
 
